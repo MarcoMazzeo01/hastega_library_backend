05b5e26e4a5db165d2d81901343da104a87aabda
plastikSun <marcom2k01@gmail.com>
2024-02-07 15:41:39 +0100

    add: bookseeder setup

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        $google_ids = [
+            'Zbe8ngEACAAJ', //harry potter 1
+            'ng--EAAAQBAJ', //regole dello shangai
+            '8_r7wpdqq5kC', //queste oscure materie
+        ];
+
+        foreach ($google_ids as $id) {
+            $book = new Book();
+            $book->google_id = $id;
+            $book->save();
+        }
+
+        for ($i = 0; $i < 4; $i++) {
+            $book = new Book();
+            $book->title = $faker->realText($maxNbChars = 30, $indexSize = 1);
+            $book->author = $faker->name();
+            $book->isbn = $faker->isbn13();
+            $book->plot = $faker->paragraph(3);
+            $book->save();
+        }
+    }
+}
